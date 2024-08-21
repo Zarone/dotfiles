c761aff5fa533c582bf583352cdc1d5ad9681aca
Zachary Alfano <redninjadolphin1@hotmail.com>
2024-08-20 23:31:42 -0400

    bug fixes

--- symlink.sh
+++ symlink.sh
@@ -1,3 +1,8 @@
 ln -s ~/dotfiles/alacritty ~/.config/alacritty
 ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/tmux ~/.config/tmux
+
+# Setup tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+tmux source-file ~/.config/tmux/tmux.conf
+tmux run-shell "~/.tmux/plugins/tpm/bin/install_plugins"
