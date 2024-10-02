a50b1ccdcb2f5c88df2921a62e0473b9b66d138a
Zachary Alfano <redninjadolphin1@hotmail.com>
2024-10-01 20:11:51 -0400

    aerospace config

--- symlink.sh
+++ symlink.sh
@@ -2,6 +2,7 @@ ln -s ~/dotfiles/alacritty ~/.config/alacritty
 ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/tmux ~/.config/tmux
 ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/dotfiles/aerospace/ ~/.config/aerospace
 
 # Setup tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
